e94d8cdf40303c81adf137a5d93441ba2b910bd8
Germanium Sculk <germaniumsculk@gmail.com>
2025-02-18 18:54:48 +0600

    mi439 : Push TARGET_DISABLES_GMS Flag

--- lineage_mi439.mk
+++ lineage_mi439.mk
@@ -28,6 +28,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 
+# Gapps
+TARGET_DISABLES_GMS := true
+
 # Boot animation resolution.
 TARGET_BOOT_ANIMATION_RES := 720
 
