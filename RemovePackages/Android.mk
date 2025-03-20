13b559213eadaa64d55d02dbfbc462b15e4b8d5d
Germanium Sculk <germaniumsculk@gmail.com>
2025-03-20 23:11:39 +0600

    mi439 : Horizondroid BringUp

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,13 +5,22 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AICorePrebuilt-aicore_20240509.01_RC02 \
     Chrome \
-    Drive \
-    Gmail2 \
+    GoogleTTS \
     Maps \
+    Photos \
+    PrebuiltGmail \
+    SoundAmplifierPrebuilt_v4.7.638126989 \
+    TrichromeLibrary-Stub \
+    TrichromeLibrary \ 
+    WallpaperAIPrebuilt_10000212 \
+    AndroidAutoStubPrebuilt \
+    DeviceIntelligenceNetworkPrebuilt-U.32_V.7_playstore_astrea_20240725.00_RC01 \
+    DevicePersonalizationPrebuiltPixel2024-U.32_V.7_playstore_aiai_20240725.00_RC08 \
     WifiResCommon_Sys \
     WifiResCommonMainline_Sys \
-    YouTube
+    Velvet 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
