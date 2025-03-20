29fa48317ab95a5c6d350afb71bbb5d8d986259d
Germanium Sculk <germaniumsculk@gmail.com>
2025-03-20 21:26:15 +0600

    mi439 : Horizondroid BringUp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/horizon/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Init
