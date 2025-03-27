5a7e117b7c51bfdf93070d8805d1346ea054a16d
Germanium Sculk <germaniumsculk@gmail.com>
2025-03-27 14:35:10 +0600

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -20,7 +20,7 @@ LOCAL_OVERRIDES_PACKAGES := \
     DevicePersonalizationPrebuiltPixel2024-U.32_V.7_playstore_aiai_20240725.00_RC08 \
     WifiResCommon_Sys \
     WifiResCommonMainline_Sys \
-    Velvet 
+    Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
